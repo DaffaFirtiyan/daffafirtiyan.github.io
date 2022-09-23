33d78c7ba9eb374c2b7f24de49e23f19acc96114
Daffa Firtiyan <mdaffa.anzalna@gmail.com>
2022-09-23 17:23:35 +1200

    Light and dark mode working
    Might need to change the colour scheme for the light mode

--- apps.js
+++ apps.js
@@ -33,6 +33,13 @@ function PageTransition() {
             element.classList.add('active');
         }
     })
+
+    // toggle between dark/light mode
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
 }
 
 PageTransition();
